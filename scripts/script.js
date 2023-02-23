56cbc545610b0ebc31f21ffe1473ac0898fefade
jose.guilherme <jose.guilherme@ikatec.com.br>
2023-02-23 00:30:06 -0300

    Configurando um contador de visitas local

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@
 //     visualizacao.innerText = contador
 // }
 
-mudaStyle =() => {
+mudaStyle = () => {
     var txtPublicados = document.getElementById('txtPublicados')
     if(txtPublicados.className === 'dropdown'){
         txtPublicados.className = 'dropdown dropdown2'
@@ -14,4 +14,61 @@ mudaStyle =() => {
         txtPublicados.className = 'dropdown'
         console.log(txtPublicados.className)
     }
+}
+var textGenerate = false
+var textGenerate2 = false
+
+chamaTexto = () => {
+    console.log("entrou na função")
+    const text01 = document.getElementById('text01')
+    const showText = document.createElement('embed');
+    const photo = document.getElementById('photo')
+
+    showText.setAttribute('src', '../docs/texto01.pdf');
+    showText.setAttribute('width', '90%');
+    showText.setAttribute('height', '1000px');
+
+    photo.style.display = 'none'
+
+    if(textGenerate === false){
+        text01.appendChild(showText)
+        textGenerate = true
+        console.log(textGenerate)
+    } else {
+        console.log('entrou aqui')
+    }
+
+}
+
+chamaTexto2 = () => {
+    console.log("entrou na função")
+    const text01 = document.getElementById('text01')
+    const showText = document.createElement('iframe');
+    const photo = document.getElementById('photo')
+
+    showText.setAttribute('src', '../docs/texto02.html');
+    showText.setAttribute('width', '90%');
+    showText.setAttribute('height', '1000px');
+    showText.style.border = 'none'
+
+    photo.style.display = 'none'
+
+    if(textGenerate2 === false){
+        text01.appendChild(showText)
+        textGenerate2 = true
+        console.log(textGenerate2)
+    } else {
+        console.log('entrou aqui')
+    }
+
+}
+
+
+teste = () => {
+        
+    if(localStorage.getItem('text01') === null){
+        localStorage.setItem('text01', true);
+       
+    }
+
 }
